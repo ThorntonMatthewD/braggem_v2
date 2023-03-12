893f94a5dbb164ccf4e0b10e592dddb195925642
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-03-12 19:06:08 -0400

    Add vite-plugin-require-transform

--- frontend/static/vite.config.ts
+++ frontend/static/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import requireTransform from 'vite-plugin-require-transform';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    requireTransform({})
   ],
   base: process.env.mode === "production" ? "/static/" : "/",
   build: {
@@ -12,5 +14,17 @@ export default defineConfig({
     emptyOutDir: true,
     manifest: true,
     modulePreload: true,
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   },
 });
+
+export type VitePluginRequireTransformParamsType = {
+  //filter files that should enter the plugin
+  fileRegex?: RegExp;
+  //prefix that would plugin into the requireSpecifier
+  importPrefix?: string;
+  //to deal with the requireSpecifier
+  importPathHandler?: Function;
+};
