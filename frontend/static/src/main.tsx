67976d1200c39e2c4512d29c5b539c03ae2520e6
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-03-13 19:29:52 -0400

    Remove strict mode so components only render once
    This prevents double-taps of some of the API calls.

--- frontend/static/src/main.tsx
+++ frontend/static/src/main.tsx
@@ -8,11 +8,9 @@ import App from "./App";
 import { AuthProvider } from "./hooks/useAuth";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
